a58ca5566e6646e77ec28f40884136967d3188e4
Dominik393 <Dominiksmykow@wp.pl>
2023-10-29 18:09:36 +0100

    Update MapDirectionTest.java

--- oolab/src/test/java/agh/ics/oop/model/MapDirectionTest.java
+++ oolab/src/test/java/agh/ics/oop/model/MapDirectionTest.java
@@ -3,20 +3,23 @@ package agh.ics.oop.model;
 import agh.ics.oop.model.MapDirection;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MapDirectionTest {
 
     @Test
     public void next(){
-        MapDirection[] directions = {MapDirection.NORTH, MapDirection.EAST, MapDirection.SOUTH, MapDirection.WEST};
+        MapDirection[] directions = MapDirection.values();
 
-        for(MapDirection direction: directions){
-            System.out.printf("Następcą %s jest %s%n", direction, direction.next());
+        for(int i = 0; i < directions.length; i++){
+            assertEquals((i),);
         }
+
     }
 
     @Test
     public void previous(){
-        MapDirection[] directions = {MapDirection.NORTH, MapDirection.EAST, MapDirection.SOUTH, MapDirection.WEST};
+        MapDirection[] directions = MapDirection.values();
 
         for(MapDirection direction: directions){
             System.out.printf("Poprzednikiem %s jest %s%n", direction, direction.previous());
