d94b7aff05a29d1615acc645a1bacac37c504508
Dominik393 <Dominiksmykow@wp.pl>
2023-11-27 16:06:41 +0100

    All Done

--- oolab/src/main/java/agh/ics/oop/model/SimulationEngine.java
+++ oolab/src/main/java/agh/ics/oop/model/SimulationEngine.java
@@ -3,9 +3,15 @@ package agh.ics.oop.model;
 import agh.ics.oop.Simulation;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class SimulationEngine {
     private ArrayList<Simulation> simulations;
+    private ArrayList<Thread> threads = new ArrayList<>();
+    private ExecutorService executorService = Executors.newFixedThreadPool(4);
+
 
     public SimulationEngine(ArrayList<Simulation> simulationss){
         this.simulations = simulationss;
@@ -18,16 +24,38 @@ public class SimulationEngine {
     }
 
     public void runAsync(){
-        ArrayList<Thread> threads = new ArrayList<>();
-
-        for(Simulation simulation: simulations){
-            Thread thread = new Thread(simulation);
+        for(Simulation simulation : this.simulations){
             threads.add(new Thread(simulation));
+        }
+
+        for(Thread thread: this.threads){
             thread.start();
         }
     }
 
     public void awaitSimulationsEnd(){
-        
+        for(Thread thread : this.threads){
+            try {
+                thread.join();
+            }catch (Exception e){
+                ;
+            }
+        }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        }catch (Exception e){
+            ;
+        }
+
+    }
+
+    public void runAsyncInThreadPool(){
+        for(Simulation simulation : simulations){
+            executorService.submit(new Thread(simulation));
+        }
+
+        executorService.shutdown();
+
     }
 }
